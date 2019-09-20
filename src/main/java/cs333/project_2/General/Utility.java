4756c01b5750b3abb0ee812549733d8ffa187def
jack broncato <jbroncato@yahoo.com>
2019-09-20 17:04:32 -0500

    general methods, need a way to generate ID

---
+++ src/main/java/cs333/project_2/General/Utility.java
@@ -0,0 +1,19 @@
+package cs333.project_2.General;
+
+public class Utility
+{
+    public static String generateID(){
+        //TODO make this generate random? string
+        return "test";
+    }
+
+    public static String searchProduct(String productname){
+        //TODO search database for product with same name
+        return "found";
+    }
+
+    public static boolean checkAvailability(String productname){
+        //TODO check that a product is available in database
+        return true;
+    }
+}
